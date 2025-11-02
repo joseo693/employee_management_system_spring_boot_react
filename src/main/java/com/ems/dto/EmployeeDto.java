554276b027eebf96bb0958a68c6f1f985e9fba64
Jose Ochoa <joseochoa@Mac.lan>
2025-11-02 17:03:20 -0500

    Created EmployeeDto, Data Transfer Object class. The Dto class will carry data between layers of the application, between the Controller, Service, and Client.

---
+++ src/main/java/com/ems/dto/EmployeeDto.java
@@ -0,0 +1,59 @@
+package com.ems.dto;
+
+// DTO Class carries data between app layers (between your Controller, Service, and Client).
+public class EmployeeDto {
+    
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+
+    // No arguments constructor
+    public EmployeeDto() {}
+
+    // Constructor
+    public EmployeeDto(Long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    // getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+}
